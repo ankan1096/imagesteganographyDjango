e2f80096fad5e45c5605c18cad96893a94b4bde3
Ankan Banerjee <ankan1096@gmail.com>
2022-04-25 13:39:08 +0530

    delete tkinter

--- steganography_website/views.py
+++ steganography_website/views.py
@@ -3,11 +3,8 @@ from .forms import EncodindForm, DecodingForm
 from django.http import HttpResponseRedirect
 import sys
 import os
-#from tkinter import *
-#from tkinter.filedialog import *
 from PIL import ImageTk
 from PIL import Image as im
-#from matplotlib import image as img
 from numpy import array
 from numpy import vectorize as vec
 from numpy import binary_repr as binary

dd5b20b6bb5098193edf9a3648f37ad97b897203
Ankan Banerjee <ankan1096@gmail.com>
2022-04-25 16:04:03 +0530

    removing imagetk

--- steganography_website/views.py
+++ steganography_website/views.py
@@ -3,7 +3,6 @@ from .forms import EncodindForm, DecodingForm
 from django.http import HttpResponseRedirect
 import sys
 import os
-from PIL import ImageTk
 from PIL import Image as im
 from numpy import array
 from numpy import vectorize as vec
